5d0801649cbc23937f8feed78171468d0d729395
Haruki1707 <72423267+Haruki1707@users.noreply.github.com>
2023-08-30 00:59:02 -0600

    Updated contactList to have reactive data + toggles

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -5,18 +5,31 @@ import {Modal} from "./Modal";
 
 export const ContactList = () => {
     const [showModal, setShowModal] = useState(false);
+    const [allContacts, setAllContacts] = useState(data);
 
-    const toogleModal = () => setShowModal(!showModal);
+    const toggleModal = () => setShowModal(!showModal);
+
+    const toggleFavorite = (id) => {
+        setAllContacts(contacts => contacts.map(contact => contact.id === id ? {
+            ...contact, isFavorite: !contact.isFavorite
+        } : contact));
+    }
+
+
+    const deleteContact = (id) => {
+        setAllContacts(contacts => contacts.filter(contact => contact.id !== id));
+    }
 
     return (<>
-        <Modal isModalActive={showModal} toggleModal={toogleModal}/>
+        <Modal isModalActive={showModal} toggleModal={toggleModal} allContacts={allContacts}
+               setAllContacts={setAllContacts}/>
         <h1 className="font-extrabold text-5xl mb-16">Agenda de contactos</h1>
         <div
             className="w-full max-w-md p-4 bg-[#284e61] border rounded-lg shadow
             sm:p-8 border-gray-700">
             <div className="flex items-center justify-between mb-4">
                 <h5 className="text-xl font-bold leading-none text-white">Registrados</h5>
-                <button type="button" onClick={toogleModal}
+                <button type="button" onClick={toggleModal}
                         className="text-white bg-gradient-to-r from-teal-400
                 via-teal-500 to-teal-600 hover:bg-gradient-to-br
                 focus:ring-4 focus:outline-none focus:ring-teal-800
@@ -31,16 +44,17 @@ export const ContactList = () => {
             </div>
             <div className="flow-root">
                 <ul className="divide-y divide-gray-400">
-                    {data.sort((contact1, contact2) => {
+                    {allContacts.sort((contact1, contact2) => {
                         return contact2.isFavorite - contact1.isFavorite;
                     }).map((data) => (<li className="py-3 sm:py-4" key={data.id}>
                         <Contact firstname={data.firstname}
                                  lastname={data.lastname}
                                  phone={data.phone}
                                  isFavorite={data.isFavorite}
-                                 defaultImg={data.defaultImg}/>
-                    </li>))
-                    }
+                                 defaultImg={data.defaultImg}
+                                 toggleFavorite={() => toggleFavorite(data.id)}
+                                 deleteContact={() => deleteContact(data.id)}/>
+                    </li>))}
                 </ul>
             </div>
         </div>
